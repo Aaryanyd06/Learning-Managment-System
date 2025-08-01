c93711371d79c0ea1e1107a2579f8e04b7f885fd
Aaryanyd06 <aaryanyd2021@gmail.com>
2025-08-01 17:25:50 +0530

    fixes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -26,7 +26,7 @@ export const clerkWebhooks = async (req, res)=>{
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.image_url
+                    imageUrl: data.image_url,
                 }
                 await User.create(userData)
                 res.json({})
@@ -37,7 +37,7 @@ export const clerkWebhooks = async (req, res)=>{
                 const userData = {
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.image_url
+                    imageUrl: data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id, userData)
                 res.json({})
