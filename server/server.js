82c00ff3e78356a132a9c733274c7152d3d5f88f
Aaryanyd06 <aaryanyd2021@gmail.com>
2025-08-03 14:52:51 +0530

    added controllers for users, courses and payment gateway

--- server/server.js
+++ server/server.js
@@ -2,18 +2,31 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import {clerkWebhooks} from './controllers/webhooks.js'
+import {clerkWebhooks, stripeWebhooks} from './controllers/webhooks.js'
 import bodyParser from 'body-parser'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkClient, clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 
 await connectDB()
+await connectCloudinary()
 
 app.use(cors())
+
+
+app.use(clerkMiddleware())
 
 app.get('/', (req,res)=>res.send("API working"))
 console.log("calling whook")
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 console.log("whook complete")
 const PORT = process.env.PORT || 5000
 
